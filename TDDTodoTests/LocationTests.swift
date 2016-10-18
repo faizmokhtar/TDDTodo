ec9db99db172b12ffd2715c43a574174cb3e8c03
Faiz Mokhtar <mfmokhtar@gmail.com>
2016-10-18 16:40:52 +0800

    Initial commit

---
+++ TDDTodoTests/LocationTests.swift
@@ -0,0 +1,77 @@
+//
+//  LocationTests.swift
+//  TDDTodo
+//
+//  Created by faiz mokhtar on 18/10/2016.
+//  Copyright © 2016 faizmokhtar. All rights reserved.
+//
+
+import XCTest
+import CoreLocation
+@testable import TDDTodo
+
+class LocationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testInit_ShouldSetName() {
+        let location = Location(name: "Test name")
+        XCTAssertEqual(location.name, "Test name", "Initializer should set the name")
+    }
+
+    func testInit_ShouldSetNameAndCoordinate() {
+        let testCoordinate = CLLocationCoordinate2D(latitude: 1, longitude: 2)
+        let location = Location(name: "", coordinate: testCoordinate)
+        XCTAssertEqual(location.coordinate?.latitude, testCoordinate.latitude, "Initializer should set latitude")
+        XCTAssertEqual(location.coordinate?.longitude, testCoordinate.longitude, "Initializer should set longitude")
+    }
+
+    func testEqualLocations_ShouldBeEqual() {
+        let firstLocation = Location(name: "Home")
+        let secondLocation = Location(name: "Home")
+        XCTAssertEqual(firstLocation, secondLocation)
+    }
+
+    func testWhenLatitudesDiffers_ShouldNotBeEqual() {
+        performNotEqualTestWithLocationProperties("Home", secondName: "Home", firstLongLat: (1.0, 0.0), secondLongLat: (0.0, 0.0))
+    }
+
+    func testWhenLongitudeDiffers_ShouldNotBeEqual() {
+        performNotEqualTestWithLocationProperties("Home", secondName: "Home", firstLongLat: (0.0, 1.0), secondLongLat: (0.0, 0.0))
+    }
+
+    func testWhenOneHasCoordinateAndTheOtherDoesnt_ShouldNotBeEqual() {
+        performNotEqualTestWithLocationProperties("Home", secondName: "Home", firstLongLat: (0.0, 1.0), secondLongLat: nil)
+    }
+
+    func testWhenNameDiffers_ShouldNotBeEqual() {
+        performNotEqualTestWithLocationProperties("Home", secondName: "Office", firstLongLat: nil, secondLongLat: nil)
+    }
+
+    func performNotEqualTestWithLocationProperties(firstName: String, secondName: String, firstLongLat: (Double, Double)?, secondLongLat: (Double, Double)?, line: UInt = #line) {
+        let firstCoordinate: CLLocationCoordinate2D?
+        if let firstLongLat = firstLongLat {
+            firstCoordinate = CLLocationCoordinate2D(latitude: firstLongLat.0, longitude: firstLongLat.1)
+        } else {
+            firstCoordinate = nil
+        }
+        let firstLocation = Location(name: firstName, coordinate: firstCoordinate)
+
+        let secondCoordinate: CLLocationCoordinate2D?
+        if let secondLongLat = secondLongLat {
+            secondCoordinate = CLLocationCoordinate2D(latitude: secondLongLat.0, longitude: secondLongLat.1)
+        } else {
+            secondCoordinate = nil
+        }
+        let secondLocation = Location(name: secondName, coordinate: secondCoordinate)
+        XCTAssertNotEqual(firstLocation, secondLocation, line: line)
+    }
+}
---
+++ TDDTodoTests/ToDoItemTests.swift
@@ -0,0 +1,83 @@
+//
+//  ToDoItemTests.swift
+//  TDDTodo
+//
+//  Created by faiz mokhtar on 17/10/2016.
+//  Copyright © 2016 faizmokhtar. All rights reserved.
+//
+
+import XCTest
+@testable import TDDTodo
+
+class ToDoItemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testInit_ShouldSetTitle() {
+        let item = ToDoItem(title: "Test title")
+        XCTAssertEqual(item.title, "Test title", "Initializer should set the item title")
+    }
+
+    func testInit_ShouldSetTitleAndDescription() {
+        let item = ToDoItem(title: "Test title", itemDescription: "Test description")
+        XCTAssertEqual(item.itemDescription, "Test description", "Initializer should set the item description")
+    }
+
+    func testInit_ShouldSetTitleAndDescriptionAndTimestamp() {
+        let item = ToDoItem(title: "Test title", itemDescription: "Test description", timestamp: 0.0)
+        XCTAssertEqual(0.0, item.timestamp, "Initializer should set the timestamp")
+    }
+
+    func testInit_ShouldSetTitleAndDescriptionAndTimestampAndLocation() {
+        let location = Location(name: "Test name")
+        let item = ToDoItem(title: "Test title", itemDescription: "Test description", timestamp: 0.0, location: location)
+        XCTAssertEqual(location.name, item.location?.name, "Initializer should set the location")
+    }
+
+    func testEqualItems_ShouldBeEqual() {
+        let firstItem = ToDoItem(title: "First")
+        let secondItem = ToDoItem(title: "First")
+        XCTAssertEqual(firstItem, secondItem)
+    }
+
+    func testWhenLocationDifferes_ShouldNotBeEqual() {
+        let firstItem = ToDoItem(title: "First title", itemDescription: "First description", timestamp: 0.0, location: Location(name: "Home"))
+        let secondItem = ToDoItem(title: "First title", itemDescription: "First description", timestamp: 0.0, location: Location(name: "Office"))
+        XCTAssertNotEqual(firstItem, secondItem)
+    }
+
+    func testWhenOneLocationIsNilAndTheOtherIsnt_ShouldNotBeEqual() {
+        var firstItem = ToDoItem(title: "First item", itemDescription: "First description", timestamp: 0.0, location: nil)
+        var secondItem = ToDoItem(title: "First item", itemDescription: "First description", timestamp: 0.0, location: Location(name: "Office"))
+        XCTAssertNotEqual(firstItem, secondItem)
+        firstItem = ToDoItem(title: "First item", itemDescription: "First description", timestamp: 0.0, location: Location(name: "Home"))
+        secondItem = ToDoItem(title: "First item", itemDescription: "First description", timestamp: 0.0, location: nil)
+        XCTAssertNotEqual(firstItem, secondItem)
+    }
+
+    func testWhenTimestampDiffers_ShouldNotBeEqual() {
+        let firstItem = ToDoItem(title: "First item", itemDescription: "First description", timestamp: 1.0, location: nil)
+        let secondItem = ToDoItem(title: "First item", itemDescription: "First description", timestamp: 0.0, location: nil)
+        XCTAssertNotEqual(firstItem, secondItem)
+    }
+
+    func testWhenDescriptionDiffers_ShouldNotBeEqual() {
+        let firstItem = ToDoItem(title: "First item", itemDescription: "First description")
+        let secondItem = ToDoItem(title: "First item", itemDescription: "Second description")
+        XCTAssertNotEqual(firstItem, secondItem)
+    }
+
+    func testWhenTitleDiffers_ShouldNotBeEqual() {
+        let firstItem = ToDoItem(title: "First item")
+        let secondItem = ToDoItem(title: "Second item")
+        XCTAssertNotEqual(firstItem, secondItem)
+    }
+}
